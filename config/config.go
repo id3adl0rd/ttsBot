10a56d4dfc2c8e8a9d95f600276e757d08d3f509
david <david.jalagania@sebbia.org>
2024-08-26 11:34:20 +0300

    Missing config file

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Bot  *BotConfig
+	Misc *MiscConfig
+}
+
+type BotConfig struct {
+	Token string
+	App   string
+	Guild string
+}
+
+type MiscConfig struct {
+	Cooldown int16
+}
+
+func NewConfig() *Config {
+	return &Config{
+		Bot:  &BotConfig{},
+		Misc: &MiscConfig{},
+	}
+}
+
+func InitConfig(path string) (*Config, error) {
+	viper.AddConfigPath(path + "/config")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := NewConfig()
+
+	cfg.Bot.App = viper.GetString("bot.app")
+	cfg.Bot.Guild = viper.GetString("bot.guild")
+	cfg.Bot.Token = viper.GetString("bot.token")
+
+	cfg.Misc.Cooldown = int16(viper.GetInt("misc.cooldown"))
+
+	Log.Info("Config successfully loaded")
+
+	return cfg, nil
+}
